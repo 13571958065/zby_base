d43ec8fcc3df1dffb1e6c8c9710d7528db2f0b99
13571958065 <531069274@qq.com>
2019-01-09 13:28:58 +0800

    项目初始化

---
+++ base-web/src/main/java/com/zby/base/baseweb/BaseWebApplication.java
@@ -0,0 +1,14 @@
+package com.zby.base.baseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (BaseWebApplication.class, args);
+    }
+
+}
+
